39cb7b8a44e9f54e6293fdebd9401339a1522610
laismanieri <lais.manieri@hotmail.com>
2023-03-14 12:36:37 -0300

    inclusão de botão Card

--- frontend/src/components/layout/Card.js
+++ frontend/src/components/layout/Card.js
@@ -1,48 +1,50 @@
 import styles from './Card.module.css'
+import { NavLink } from 'react-router-dom';
 
 function Card(){
+    
 
     return(
 
 <div className={styles.divCard}>
         <div className={styles.row}> 
-         
             <div className={styles.card}>
-                
                 <img className={styles.image} src='img/e-assim-que-comeca.jpg' alt=''/>
-                <div className={styles.titulo}><h4><b>É assim que Começa</b></h4></div>
-                <p>R$ 39,90</p>
-                <button className={styles.button}>Comprar</button>
+                <div>
+                    <p className={styles.titulo}>É assim que Começa</p></div>
+                    <p className={styles.preco}>R$ 39,90</p>
+                    <button className={styles.button}><NavLink to='/carrinho' >Comprar</NavLink>
+                    </button>
             </div>  
             <div className={styles.card}>
                 <img className={styles.image} src="img/mil-batidas-do-coracao.jpg" alt=""/>
-                <h4><b>Mil batidas do coração</b></h4>
-                <p>R$ 50,92</p>
-                <button className={styles.button}>Comprar</button>
+                <p className={styles.titulo}>Mil batidas do coração</p>
+                <p className={styles.preco}>R$ 50,92</p>
+                <button className={styles.button}><NavLink to='/carrinho' >Comprar</NavLink></button>
             </div> 
             <div className={styles.card}>
                 <img className={styles.image} src="img/o-conde-de-monte-cristo.jpg" alt=""/>
-                <h4><b>O conde de monte Cristo</b></h4>
-                <p> R$ 111,90</p>
-                <button className={styles.button}>Comprar</button>
+                <p className={styles.titulo}>O conde de monte Cristo</p>
+                <p className={styles.preco}> R$ 111,90</p>
+                <button className={styles.button}><NavLink to='/carrinho' >Comprar</NavLink></button>
             </div> 
             <div className={styles.card}>
                 <img className={styles.image} src="img/orgulho-e-preconceito.png" alt=""/>
-                <h4><b>Orgulho e preconceito</b></h4>
-                <p>R$ 31,40</p>
-                <button className={styles.button}>Comprar</button>
+                <p className={styles.titulo}>Orgulho e preconceito</p>
+                <p className={styles.preco}>R$ 31,40</p>
+                <button className={styles.button}><NavLink to='/carrinho' >Comprar</NavLink></button>
             </div> 
             <div className={styles.card}>
                 <img className={styles.image} src="img/corte-de-espinhos.jpg" alt=""/>
-                <h4><b>Corte de espinhos e rosas</b></h4>
-                <p>R$ 36,01</p>
-                <button className={styles.button}>Comprar</button>
+                <p className={styles.titulo}>Corte de espinhos e rosas</p>
+                <p className={styles.preco}>R$ 36,01</p>
+                <button className={styles.button}><NavLink to='/carrinho' >Comprar</NavLink></button>
             </div> 
             <div className={styles.card}>
                 <img className={styles.image} src="img/corte-de-espinhos.jpg" alt=""/>
-                <h4><b>Corte de espinhos e rosas</b></h4>
-                <p>R$ 36,01</p>
-                <button className={styles.button}>Comprar</button>
+                <p className={styles.titulo}>Corte de espinhos e rosas</p>
+                <p className={styles.preco}>R$ 36,01</p>
+                <button className={styles.button}><NavLink to='/carrinho' >Comprar</NavLink></button>
             </div> 
         </div>
     </div>
