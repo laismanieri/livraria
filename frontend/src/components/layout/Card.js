5c32eaca97aa4f6514e411b2b770e153bb0c257b
laismanieri <lais.manieri@hotmail.com>
2023-03-13 22:37:32 -0300

    Inclusão de botão no card

--- frontend/src/components/layout/Card.js
+++ frontend/src/components/layout/Card.js
@@ -8,34 +8,41 @@ function Card(){
         <div className={styles.row}> 
          
             <div className={styles.card}>
+                
                 <img className={styles.image} src='img/e-assim-que-comeca.jpg' alt=''/>
-                <h4><b>É assim que Começa</b></h4>
+                <div className={styles.titulo}><h4><b>É assim que Começa</b></h4></div>
                 <p>R$ 39,90</p>
+                <button className={styles.button}>Comprar</button>
             </div>  
             <div className={styles.card}>
                 <img className={styles.image} src="img/mil-batidas-do-coracao.jpg" alt=""/>
                 <h4><b>Mil batidas do coração</b></h4>
                 <p>R$ 50,92</p>
+                <button className={styles.button}>Comprar</button>
             </div> 
             <div className={styles.card}>
                 <img className={styles.image} src="img/o-conde-de-monte-cristo.jpg" alt=""/>
                 <h4><b>O conde de monte Cristo</b></h4>
                 <p> R$ 111,90</p>
+                <button className={styles.button}>Comprar</button>
             </div> 
             <div className={styles.card}>
                 <img className={styles.image} src="img/orgulho-e-preconceito.png" alt=""/>
                 <h4><b>Orgulho e preconceito</b></h4>
                 <p>R$ 31,40</p>
+                <button className={styles.button}>Comprar</button>
             </div> 
             <div className={styles.card}>
                 <img className={styles.image} src="img/corte-de-espinhos.jpg" alt=""/>
                 <h4><b>Corte de espinhos e rosas</b></h4>
                 <p>R$ 36,01</p>
+                <button className={styles.button}>Comprar</button>
             </div> 
             <div className={styles.card}>
                 <img className={styles.image} src="img/corte-de-espinhos.jpg" alt=""/>
                 <h4><b>Corte de espinhos e rosas</b></h4>
                 <p>R$ 36,01</p>
+                <button className={styles.button}>Comprar</button>
             </div> 
         </div>
     </div>
